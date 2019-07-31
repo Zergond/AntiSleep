2fc64227f1e8c8edcb6c923725dc0e8266d6c1fe
Pavel Kulesha <azimut965@gmail.com>
2019-07-31 20:30:58 +0300

    AntiSleep v1.0

---
+++ AntiSleep/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Runtime.InteropServices;
+
+namespace AntiSleep
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            while (true)
+            {
+                Thread.Sleep(60000);
+                PressShift();
+            }
+        }
+        static void PressShift()
+        {
+            InputSimulator inputSimulator = new InputSimulator();
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.SHIFT);
+        }
+
+    }
+}
